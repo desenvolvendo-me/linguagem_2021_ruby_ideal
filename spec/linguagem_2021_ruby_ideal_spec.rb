2b1d151793c21c347df8a4a2ab8b60f0dd56d0ea
Marco Castro <marcodotcastro@gmail.com>
2021-04-21 09:56:19 -0300

    build: config gem

--- spec/linguagem_2021_ruby_ideal_spec.rb
+++ spec/linguagem_2021_ruby_ideal_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Linguagem2021RubyIdeal do
+
   it "has a version number" do
     expect(Linguagem2021RubyIdeal::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
